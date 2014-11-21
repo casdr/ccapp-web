ceffda9970ed0b3e91f21a5f8cd40c72f61820e7
milo526 <milocesar1@gmail.com>
2014-11-21 14:26:07 +0100

    Added Progressbar for Teachers

--- templates/teacher-table.php
+++ templates/teacher-table.php
@@ -7,5 +7,13 @@
     {{#break}}
       <tr class="tr-break"><td>{{start}} - {{end}}</td><td>{{lesson}}</td><td>{{room}}</td><td>{{class}}</td></tr>
     {{/break}}
+    {{#current}}
+    <tr><td colspan="4">
+      <div class="progress">
+        <div class="progress-bar" role="progressbar" aria-valuenow="{{percent}}" aria-valuemin="0" aria-valuemax="100" style="width: {{percent}}%">
+        </div>
+      </div>
+    </td></tr>
+    {{/current}}
   {{/lessons}}
 </table>
