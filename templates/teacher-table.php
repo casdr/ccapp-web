372d2870f97db3d926582a6dcc5f817d3e46e6b1
Cas de Reuver <me@cas.yt>
2015-01-26 11:36:48 +0100

    Added groups for teachers

--- templates/teacher-table.php
+++ templates/teacher-table.php
@@ -1,5 +1,5 @@
 <table class="table">
   {{#.}}
-    <tr {{#break}}class="tr-break"{{/break}}{{#test}}class="tr-test"{{/test}}><td>{{start}} - {{end}}</td><td>{{class}}</td><td>{{room}}</td><td>{{teacher}}</td></tr>
+    <tr {{#break}}class="tr-break"{{/break}}{{#test}}class="tr-test"{{/test}}><td>{{start}} - {{end}}</td><td>{{class}}</td><td>{{room}}</td><td>{{groups}}</td></tr>
   {{/.}}
 </table>
