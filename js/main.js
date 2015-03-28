a63982852f919b05ac69ecebaf337be834fef8df
Cas de Reuver <me@cas.yt>
2015-03-28 11:31:18 +0000

    New piwik

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@ $(document).ready(function() {
     _paq.push(["enableLinkTracking"]);
 
     (function() {
-        var u="https://nl.hostedpiwik.website/";
+        var u="//nl.hostedpiwik.website/";
         _paq.push(["setTrackerUrl", u+"piwik.php"]);
-        _paq.push(["setSiteId", "28"]);
+        _paq.push(["setSiteId", "6"]);
         var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
         g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
     })();
