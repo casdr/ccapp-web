c6dc8f71368353173858314385e43b7de0215c88
Cas de Reuver <me@cas.yt>
2015-03-28 11:36:45 +0000

    That didn't work

--- js/main.js
+++ js/main.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
     var currentweek;
     var current;
-    var _paq = _paq || [];
-    _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
-    _paq.push(["setCookieDomain", "*.ccweb.nl"]);
-    _paq.push(["trackPageView"]);
-    _paq.push(["enableLinkTracking"]);
-
-    (function() {
-        var u="//nl.hostedpiwik.website/";
-        _paq.push(["setTrackerUrl", u+"piwik.php"]);
-        _paq.push(["setSiteId", "6"]);
-        var d=document, g=d.createElement("script"), s=d.getElementsByTagName("script")[0]; g.type="text/javascript";
-        g.defer=true; g.async=true; g.src=u+"piwik.js"; s.parentNode.insertBefore(g,s);
-    })();
     function getSchedule(id, week, goToToday) {
         var days = ['maandag', 'dinsdag', 'woensdag', 'donderdag', 'vrijdag'];
         var dow = new Date().getDay();
